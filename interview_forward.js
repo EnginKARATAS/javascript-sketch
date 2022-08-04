d21d28f6e6b76800c2f62404f7e45f0adf34a74a
Engin Karataş <enginkaratas99@gmail.com>
2022-08-04 20:43:15 +0300

    Anagram

--- interview_forward.js
+++ interview_forward.js
@@ -10,7 +10,7 @@ function fibonacci(n) {
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-console.log(fibonacci(5));
+// console.log(fibonacci(5));
 
 //
 //palindrome
@@ -19,5 +19,29 @@ let isPalindrome = (text) => {
   //fof
   return text === text.split("").reverse().join("");
 };
-console.log(isPalindrome("fof"))
-console.log(isPalindrome("foo"))
+// console.log(isPalindrome("fof"));
+// console.log(isPalindrome("foo"));
+
+//
+//Anagram
+//
+const anagram = (str1, str2) => {
+  if (str1.length !== str2.length) {
+    return false
+  }
+  const str1Lower = str1.toLowerCase()
+  const str2Lower = str2.toLowerCase()
+
+  if (str1Lower == str2Lower) {
+    return false
+  }
+
+  const str1AlphabeticSort = str1Lower.split("").sort((a,b)=>a>b?1:-1).join("")
+  const str2AlphabeticSort = str2Lower.split("").sort((a,b)=>a>b?1:-1).join("")
+  if (str1AlphabeticSort === str2AlphabeticSort) {
+    return true
+  }
+  return false
+  
+};
+console.log(anagram("engin","engni"))
