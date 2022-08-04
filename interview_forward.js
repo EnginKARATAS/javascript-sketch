a520ef0d2a116d6e6b71a6ae92b358d393727aa2
Engin Karataş <enginkaratas99@gmail.com>
2022-08-04 19:46:41 +0300

    fibonacci

--- interview_forward.js
+++ interview_forward.js
@@ -1,1 +1,13 @@
-!
+//
+//fibonacci
+//
+//0,1,1,2,3,5,8,13,21... givin n
+
+function fibonacci(n) {
+  if (n < 2) {
+    return 1;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(5)) 
