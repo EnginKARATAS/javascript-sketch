1505ebb0de09ca5befca247df9acaccf8217963b
Engin Karataş <enginkaratas99@gmail.com>
2022-08-04 20:00:36 +0300

    Polindrome

--- interview_forward.js
+++ interview_forward.js
@@ -10,4 +10,14 @@ function fibonacci(n) {
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-console.log(fibonacci(5)) 
+console.log(fibonacci(5));
+
+//
+//palindrome
+//
+let isPalindrome = (text) => {
+  //fof
+  return text === text.split("").reverse().join("");
+};
+console.log(isPalindrome("fof"))
+console.log(isPalindrome("foo"))
