cdd2a233b81712f5a79b7aaf8aef67e1ee9f8cb4
Engin Karataş <enginkaratas99@gmail.com>
2022-08-04 14:26:21 +0300

    Split each new sentence to a separate line in the paragraph text. A sentence text could terminated with a period

--- interview_dom.js
+++ interview_dom.js
@@ -7,13 +7,17 @@ paragraph.innerHTML = paragraph.innerHTML
   .split(" ")
   .map((item) =>
     item.length > 8 ? `<span style="background:yellow">${item}</span>` : item
-  ).join(" ");
-
+  )
+  .join(" ");
 
 //Add a link bact to the source of the text after the paragraph tag.
-const link = document.createElement('a')
-link.href = "www.google.com"
-const linkP = document.createElement('p')
-linkP.innerHTML = "lorem ipsum click"
-link.append(linkP)
-document.body.append(link)
+const link = document.createElement("a");
+link.href = "www.google.com";
+const linkP = document.createElement("p");
+linkP.innerHTML = "lorem ipsum click";
+link.append(linkP);
+// document.body.append(link);
+
+//Split each new sentence to a separate line in the paragraph text. A sentence text could terminated with a period
+const parag = document.querySelector("p");
+parag.innerHTML = parag.innerHTML.split(".").join(".</p><p>") + "</p>";
