7021c48d57dbc57978b38ac9ac6af0996e4230f0
Engin Karataş <enginkaratas99@gmail.com>
2024-01-07 21:09:55 +0300

    Update 3index.js

--- 2024/3index.js
+++ 2024/3index.js
@@ -9,6 +9,17 @@ function countNumbers() {
 
 //closure that mean make fields private and call them from external call with writing some functions inside the closure function
 const numbersAPI = countNumbers();
+console.log(numbersAPI.getValue())
 console.log(numbersAPI.increaseValue())
 console.log(numbersAPI.getValue())
  
+//closure scenario
+const tokenManager = () =>{
+    const token = "foo"
+    return () => token
+}
+tokenAPI = tokenManager();
+const showTheClosureFromPrototypeMenuClosure = tokenAPI
+const resultOfToken = tokenAPI()
+console.dir(showTheClosureFromPrototypeMenuClosure)
+console.log("🚀 ~ file: 3index.js:23 ~ resultOfToken:", resultOfToken)
