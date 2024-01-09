b11495bf6779c68d7ff0a7203d3587a3487bcfc7
Engin Karataş <enginkaratas99@gmail.com>
2024-01-09 14:41:13 +0300

    js prototype vs js classes

--- 2024/20.Classes.js
+++ 2024/20.Classes.js
@@ -1,6 +1,6 @@
 //design a class for employee whick takes id an name in during constuction of object and has salary property
 class Employee {
-  constructor(id, name, department) {
+  constructor(id, name) {
     if (!id || !name) throw Error("name or id fiels missing");
     this.id = id;
     this.name = name;
@@ -36,3 +36,30 @@ console.log(employee.name, " ", employee.id, " ", employee.salary);
 const manager = new Manager(114, "Jasica")
 manager.setDepartment("IT");
 console.log(manager);
+
+//Write with prototype syntax
+console.log("---prototype---");
+var Employee2 = function (id, name){
+  if (!id || !name) throw Error("name or id fiels missing");
+    this.id = id;
+    this.name = name;
+}
+
+Employee2.prototype.getId = function() {
+  return this.id
+}
+
+Employee2.prototype.getName = function() {
+  return this.name;
+}
+
+Employee2.prototype.getSalary = function() {
+  return this.salary;
+}
+Employee2.prototype.setSalary = function (salary) {
+  this.salary = salary;
+}
+
+const employee2 = new Employee2(113, "Rufus");
+employee.setSalary(120000)
+console.log(employee.name, " ", employee.id, " ", employee.salary);
