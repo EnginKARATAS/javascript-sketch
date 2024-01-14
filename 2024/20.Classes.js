37f5886fdab83d0449f833a40eb07239df51099c
Engin Karataş <enginkaratas99@gmail.com>
2024-01-14 22:49:23 +0300

    update syntax in files

--- 2024/20.Classes.js
+++ 2024/20.Classes.js
@@ -20,46 +20,47 @@ class Employee {
   }
 }
 
-class Manager extends Employee{
-    setDepartment(department){
-        this.department = department;
-    }
-    getDepartment(){
-        return this.department;
-    }
+class Manager extends Employee {
+  setDepartment(department) {
+    this.department = department;
+  }
+  getDepartment() {
+    return this.department;
+  }
 }
 
 const employee = new Employee(113, "Rufus");
-employee.setSalary(120000)
+employee.setSalary(120000);
 console.log(employee.name, " ", employee.id, " ", employee.salary);
 
-const manager = new Manager(114, "Jasica")
+const manager = new Manager(114, "Jasica");
 manager.setDepartment("IT");
 console.log(manager);
 
 //Write with prototype syntax
 console.log("---prototype---");
-var Employee2 = function (id, name){
+var Employee2 = function (id, name) {
   if (!id || !name) throw Error("name or id fiels missing");
-    this.id = id;
-    this.name = name;
-}
+  this.id = id;
+  this.name = name;
+};
 
-Employee2.prototype.getId = function() {
-  return this.id
-}
+Employee2.prototype.getId = function () {
+  return this.id;
+};
 
-Employee2.prototype.getName = function() {
+Employee2.prototype.getName = function () {
   return this.name;
-}
+};
 
-Employee2.prototype.getSalary = function() {
+Employee2.prototype.getSalary = function () {
   return this.salary;
-}
+};
+
 Employee2.prototype.setSalary = function (salary) {
   this.salary = salary;
-}
+};
 
 const employee2 = new Employee2(113, "Rufus");
-employee.setSalary(120000)
+employee.setSalary(120000);
 console.log(employee.name, " ", employee.id, " ", employee.salary);
