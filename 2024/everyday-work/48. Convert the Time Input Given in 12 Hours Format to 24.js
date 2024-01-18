261a84272563d48f4e433fd8206e4b776d28510b
Engin Karataş <enginkaratas99@gmail.com>
2024-01-18 19:21:23 +0300

    convert the time input given in 24 hours format to 12 hours format

---
+++ 2024/everyday-work/48. Convert the Time Input Given in 12 Hours Format to 24.js
@@ -0,0 +1,25 @@
+// Write a function which can convert the time input given in 24 hours format to 12 hours format
+// convertToZ4HrsFormat("12:10AM"); // 00:10
+// convertToZ4HrsFormat("5:00AM"); // 05:00
+// convertToZ4HrsFormat("12:33PM"); // 12:33
+// convertToZ4HrsFormat("01:59PM"); // 13:59
+// convertToZ4HrsFormat("11:8PM"); // 23:08
+// convertToZ4HrsFormat("10:02PM"); // 22:02
+
+const convertToZ4HrsFormat = (timeText) => {
+  let leftTwo = timeText.toLowerCase().split("").slice(0, 2);
+  let rightTwo = timeText.toLowerCase().split("").slice(3, 5);
+  let amOrPm = timeText.toLowerCase().split("").slice(5, 7);
+
+  let numberLeft = +leftTwo.join("");
+  let numberRight = +rightTwo.join("");
+  let stringAmOrPm = +amOrPm.join("");
+
+  if (numberLeft > 12 && numberLeft <= 24) {
+    let bigger12 = numberLeft % 12;
+    numberLeft = bigger12
+  }
+  return `${numberLeft}:${numberRight}`;
+};
+
+console.log(convertToZ4HrsFormat("21:29"));
