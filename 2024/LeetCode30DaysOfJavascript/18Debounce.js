d4a6ad8081793bfcea69008a63b258e4130aaa32
Engin Karataş <enginkaratas99@gmail.com>
2024-02-23 22:01:18 +0300

    Create 18Debounce.js

---
+++ 2024/LeetCode30DaysOfJavascript/18Debounce.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function (fn, t) {
+    let timeoutId;
+    return function (...args) {
+        if (timeoutId)
+            clearTimeout(timeoutId)
+        timeoutId = setTimeout(() => fn.apply(this, args), t)
+        // clear and run always, when last func will comes, execute last one
+        //execute fn args after t sec
+
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
