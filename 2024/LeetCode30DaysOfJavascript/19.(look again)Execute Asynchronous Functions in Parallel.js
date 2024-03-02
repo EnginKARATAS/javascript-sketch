11e3b9a651c2d7b3d29a0481b520d22ca91b176e
Engin Karataş <enginkaratas99@gmail.com>
2024-03-02 21:38:47 +0300

    Update 19.(look again)Execute Asynchronous Functions in Parallel.js

--- 2024/LeetCode30DaysOfJavascript/19.(look again)Execute Asynchronous Functions in Parallel.js
+++ 2024/LeetCode30DaysOfJavascript/19.(look again)Execute Asynchronous Functions in Parallel.js
@@ -23,7 +23,7 @@ var promiseAll = function (functions) {
  * promise.then(console.log); // [42]
  */
 // Given an array of asynchronous functions functions, return a new promise promise. Each function in the array accepts no arguments and returns a promise. All the promises should be executed in parallel.
-
+ 
 // promise resolves:
 
 // When all the promises returned from functions were resolved successfully in parallel. The resolved value of promise should be an array of all the resolved values of promises in the same order as they were in the functions. The promise should resolve when all the asynchronous functions in the array have completed execution in parallel.
