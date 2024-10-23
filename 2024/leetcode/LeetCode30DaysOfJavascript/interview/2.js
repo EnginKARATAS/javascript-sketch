b3a4504c44e2e3bba09b02d7843e910c22512b8f
Engin Karatas <enginkaratas99@gmail.com>
2024-10-23 13:18:27 +0300

    find escaped characters #!b#

---
+++ 2024/leetcode/LeetCode30DaysOfJavascript/interview/2.js
@@ -0,0 +1,17 @@
+function findEscaped(text) {
+  let insideHash = false;
+  let counter = 0;
+
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === "#") {
+      insideHash = !insideHash;
+    }
+ 
+      if (insideHash && text[i] === "!" && /[a-z]/.test(text[i + 1])) counter++;
+  
+  }
+  console.log(counter);
+}
+
+findEscaped("a!de#dwx!re!e##!##sdc!a!f");
+findEscaped("#a!b#c#d!e!f#g!");
